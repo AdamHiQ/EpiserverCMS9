d878adefdff6a6d25a58b77471d7226081c84367
AdamHiQ <adam.lundborg@hiq.se>
2016-10-21 16:21:13 +0200

    Adding the project

---
+++ Business/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Episerver9.Business
+{
+    [InitializableModule]
+    public class BundleConfig : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            if(context.HostType == HostType.WebApplication)
+            {
+                RegisterBundles(BundleTable.Bundles);
+            }
+        }
+
+        public void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery.js").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap.js").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+            bundles.Add(new StyleBundle("~/Content/style.css").Include("~/Content/bootstrap.css"));
+
+            BundleTable.EnableOptimizations = false;
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
